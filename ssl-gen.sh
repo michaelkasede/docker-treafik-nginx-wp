622f249eaebfdba6fd0e4c160148a53ea0cc0cf8
Michael <mckas2001@gmail.com>
2023-03-04 19:22:44 +0300

    changed folder name where ssl certificate will be saved.

--- ssl-gen.sh
+++ ssl-gen.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Prompt user for domain name
-read -p "Enter the domain name for which the SSL certificate will be generated: " domain_name
-mkdir -p keys
+read -p "Enter the domain name for which the SSL certificate will be generated: " domain
+mkdir -p certs
 # Generate private key and self-signed certificate
-openssl req -newkey rsa:2048 -nodes -keyout "keys/${domain_name}.key" \
-    -x509 -days 365 -out "keys/${domain_name}.pem" \
-    -subj "/CN=${domain_name}"
+openssl req -newkey rsa:2048 -nodes -keyout "certs/${domain}.key" \
+    -x509 -days 365 -out "certs/${domain}.pem" \
+    -subj "/CN=${domain}"
 
 # Print success message
-echo "Self-signed SSL certificate generated for ${domain_name}!"
+echo "Self-signed SSL certificate generated for ${domain}!"
